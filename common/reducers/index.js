2505f0ed3d46ef230957daf5b1d8e6cc2d014c72
Lucas <lucas@agtlucas.com>
2016-02-21 09:18:04 -0300

    Combine reducers

---
+++ common/reducers/index.js
@@ -0,0 +1,11 @@
+// JUST A REDUX EXAMPLE!!
+// I'LL CHANGE THIS VERY SOON!
+
+import { combineReducers } from 'redux'
+import counter from './counter'
+
+const rootReducer = combineReducers({
+  counter
+})
+
+export default rootReducer
