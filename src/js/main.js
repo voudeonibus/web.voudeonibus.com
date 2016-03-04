c4a45f9bf437aeee0d77451b0cd538e04f9f630d
Lucas <lucas@agtlucas.com>
2016-03-04 00:42:04 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
+
+import About from './about'
 import '../styles/main.scss'
 
 export default class App extends Component {
   render () {
     return (
-      <div>App</div>
+      <div>
+        <a href='/about'>About</a>
+      </div>
     )
   }
 }
 
-render(<App />, document.getElementById('the__root'))
+render(
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <Route path='about' componet={About} />
+    </Route>
+  </Router>,
+  document.getElementById('the__root')
+)
