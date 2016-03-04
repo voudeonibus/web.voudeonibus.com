de10dc9ff8c01f2774075c0e9251e55c873a7fff
Lucas <lucas@agtlucas.com>
2016-03-04 08:10:38 -0300

    Update contact.js

---
+++ src/js/contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+export default class Contact extends Component {
+    render () {
+      return (
+        <div>
+          <Link to='/'>Home</Link>
+        </div>
+      )
+    }
+}
