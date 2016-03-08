2f6f8c98f55db748b668efc3f3a9749f3241fe8c
Lucas <lucas@agtlucas.com>
2016-03-08 09:19:02 -0300

    Cleaning up LineList.js

--- src/js/LineList.js
+++ src/js/LineList.js
@@ -1,84 +1,82 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router'
 
 import VoudeOnibusPng from '../img/voudeonibus.png'
-
 
 export default class LineList extends Component {
     render () {
       return (
-        <ul className="vdb-line-list">
+        <ul className='vdb-line-list'>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-line-active vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
           <li className='vdb-line-list_item vdb-favorite'>
-              <a href=''>
-                <span className="number"><span>200</span></span>
-                Três Rios
-              </a>
+            <a href=''>
+              <span className='number'><span>200</span></span>
+              Três Rios
+            </a>
           </li>
         </ul>
       )
