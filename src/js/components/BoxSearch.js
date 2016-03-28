5b44a91873484893a58aad60b2dcac828a022d04
Danilo W <danilo_wzn@hotmail.com>
2016-03-28 13:35:56 -0300

    Placeholder mobile

--- src/js/components/BoxSearch.js
+++ src/js/components/BoxSearch.js
@@ -4,7 +4,7 @@ export default class BoxSearch extends Component {
   render () {
     return (
       <form className='vdb-search-box'>
-        <input type='text' placeholder='Busque por linhas ou por variações' />
+        <input type='text' placeholder='Busque pela sua linha' />
       </form>
     )
   }
