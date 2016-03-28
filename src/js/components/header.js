71c1ca9281825a8ed3aa8fb37a7e4a2ec1d71589
Danilo W <danilo_wzn@hotmail.com>
2016-03-28 13:32:49 -0300

    New breackpoint

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -5,14 +5,13 @@ import VoudeOnibusHorizontalPng from '../../img/voudeonibus-horizontal.png'
 
 const imgs = `${VoudeOnibusHorizontalPng} 640w, ${VoudeOnibusPng} 800w`
 
-export default class About extends Component {
+export default class Header extends Component {
   render () {
     return (
       <header className='vdb-header'>
         <h1 className='vdb-title'>
           <img alt='Your picture description' srcSet={imgs} />
         </h1>
-
       </header>
     )
   }
