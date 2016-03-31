9348ade748597060a59c453a0f6ae5bc9c3e31f4
Lucas <lucas@agtlucas.com>
2016-03-31 09:36:23 -0300

    Update LineItemHeader - string interpolation

--- src/js/components/LineItemHeader.js
+++ src/js/components/LineItemHeader.js
@@ -5,7 +5,7 @@ import HeaderPlaceholder from '../../pics/header-placeholder.png'
 export default class LineItemHeader extends Component {
   render () {
     return (
-      <div className='vdb-line-item-header' style={{backgroundImage: 'url('+HeaderPlaceholder+')'}}>
+      <div className='vdb-line-item-header' style={{backgroundImage: `url('${HeaderPlaceholder}')`}}>
         <div className='vdb-line-item-header_infos'>
           <h3 className='name'>
             <span className='number'><span>200</span></span>
