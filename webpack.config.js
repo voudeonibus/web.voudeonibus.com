55908a7ecc47cc3f79dbd31e9ad6c7582cbb55e8
Lucas <lucas@agtlucas.com>
2016-04-03 19:36:04 -0300

    Update dev webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   devtool: 'eval-source-map',
   entry: [
     'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'src/js/app.js')
+    path.join(__dirname, 'src/js/index.js')
   ],
 
   output: {
