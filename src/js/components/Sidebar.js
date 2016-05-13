8c929bee9c2e3f49d26a6c11cc40b7d80042cb93
Lucas <lucas@agtlucas.com>
2016-05-13 06:41:37 -0300

    JS Standard

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -4,53 +4,52 @@ import classNames from 'classnames'
 import { Router, Route, Link, browserHistory } from 'react-router'
 
 let listLines = [
-  { lineName: "Três Rios", lineNumber: "100"  }
-  ,{ lineName: "Três Rios", lineNumber: "100"  }
-  ,{ lineName: "Três Rios", lineNumber: "100"  }
-  ,{ lineName: "Sábado", lineNumber: "100"  }
-  ,{ lineName: "Sábado", lineNumber: "100"  }
-  ,{ lineName: "Sábado", lineNumber: "100"  }
-  ,{ lineName: "Dômingís e feriados", lineNumber: "999"  }
-  ,{ lineName: "Domingos e feriados", lineNumber: "100"  }
-  ,{ lineName: "Domingos e feriados", lineNumber: "100"  }
-  ,{ lineName: "Domingos e feriados", lineNumber: "100"  }
-  ,{ lineName: "Domingos e feriados", lineNumber: "100"  }
-  ,{ lineName: "Domingos e feriados", lineNumber: "100"  }
-  ,{ lineName: "Domingos e feriados", lineNumber: "100"  }
-  ,{ lineName: "Domingos e feriados", lineNumber: "100"  }
-  ,{ lineName: "Schroeder", lineNumber: "100"  }
-  ,{ lineName: "Kohlbach", lineNumber: "100"  }
-  ,{ lineName: "Czerniewicz", lineNumber: "100"  }
-];
+  { lineName: 'Três Rios', lineNumber: '100' },
+  { lineName: 'Três Rios', lineNumber: '100' },
+  { lineName: 'Três Rios', lineNumber: '100' },
+  { lineName: 'Sábado', lineNumber: '100' },
+  { lineName: 'Sábado', lineNumber: '100' },
+  { lineName: 'Sábado', lineNumber: '100' },
+  { lineName: 'Dômingís e feriados', lineNumber: '999' },
+  { lineName: 'Domingos e feriados', lineNumber: '100' },
+  { lineName: 'Domingos e feriados', lineNumber: '100' },
+  { lineName: 'Domingos e feriados', lineNumber: '100' },
+  { lineName: 'Domingos e feriados', lineNumber: '100' },
+  { lineName: 'Domingos e feriados', lineNumber: '100' },
+  { lineName: 'Domingos e feriados', lineNumber: '100' },
+  { lineName: 'Domingos e feriados', lineNumber: '100' },
+  { lineName: 'Schroeder', lineNumber: '100' },
+  { lineName: 'Kohlbach', lineNumber: '100' },
+  { lineName: 'Czerniewicz', lineNumber: '100' }
+]
 
-let strangeNames = ["Schroeder","Kohlbach","Czerniewicz","Corticeira","Sei lá","Woznica"]
+let strangeNames = ['Schroeder', 'Kohlbach', 'Czerniewicz', 'Corticeira', 'Sei lá', 'Woznica']
 
 class MenuItemLines extends Component {
-  render(){
-
-    let liClass = classNames ({
+  render (){
+    let liClass = classNames({
       'vdb-line-list_item': true,
       'vdb-favorite': false,
       'vdb-line-active': this.props.active
-    });
+    })
 
-    var slug = function(str) {
-      str = str.replace(/^\s+|\s+$/g, ''); // trim
-      str = str.toLowerCase();
+    var slug = function (str) {
+      str = str.replace(/^\s+|\s+$/g, '') // trim
+      str = str.toLowerCase()
 
       // remove accents, swap ñ for n, etc
-      var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
-      var to   = "aaaaaeeeeeiiiiooooouuuunc------";
-      for (var i=0, l=from.length ; i<l ; i++) {
-        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+      var from = 'ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;'
+      var to = 'aaaaaeeeeeiiiiooooouuuunc------'
+      for (var i = 0, l = from.length; i < l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
       }
 
       str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
         .replace(/\s+/g, '-') // collapse whitespace and replace by -
-        .replace(/-+/g, '-'); // collapse dashes
+        .replace(/-+/g, '-') // collapse dashes
 
-      return str;
-    };
+      return str
+    }
 
     return (
       <li className={liClass}>
@@ -81,32 +80,32 @@ export default class LineList extends Component {
     }
   }
 
-  setSearch(query) {
+  setSearch (query) {
     this.setState({
       searchQ: query
     })
   }
 
-  handleLines(index) {
+  handleLines (index) {
     this.setState({
       selectedIndex: index
     })
   }
 
   render () {
-    let filterData, contentLines;
-    let searchString = this.props.searchQ;
-    if(isNaN(searchString)){
-        filterData = listLines.filter(function(l){
-          return _.deburr(l.lineName.toLowerCase()).match(_.deburr(searchString.toLowerCase()))
-        });
+    let filterData, contentLines
+    let searchString = this.props.searchQ
+    if (isNaN(searchString)) {
+      filterData = listLines.filter(function (l) {
+        return _.deburr(l.lineName.toLowerCase()).match(_.deburr(searchString.toLowerCase()))
+      })
     } else {
-        filterData = listLines.filter(function(l){
-            return l.lineNumber.match( searchString );
-        });
+      filterData = listLines.filter(function (l) {
+        return l.lineNumber.match(searchString)
+      })
     }
 
-    if(filterData.length) {
+    if (filterData.length) {
       contentLines = filterData.map((item, index) => {
         return <MenuItemLines content={item}
               key={index}
@@ -115,13 +114,13 @@ export default class LineList extends Component {
       })
     } else {
       let strangeNamesList = strangeNames.map((name, index) => {
-        let ent;
-        if(index == strangeNames.length - 2) {
-          ent = " ou "
-        } else if(index == strangeNames.length - 1) {
-          ent = ""
+        let ent
+        if (index === strangeNames.length - 2) {
+          ent = ' ou '
+        } else if (index === strangeNames.length - 1) {
+          ent = ''
         } else {
-          ent = ", "
+          ent = ', '
         }
         return <span><a key={index} className='strange-name' href='javascript:void(0)' onClick={this.setSearch.bind(this, name)}>{name}</a>{ent}</span>
       })
