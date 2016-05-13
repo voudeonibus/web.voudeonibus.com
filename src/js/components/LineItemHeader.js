157779c21aa3971ee6e560f64678e676c135a21c
Lucas <lucas@agtlucas.com>
2016-05-13 06:32:31 -0300

    JS Standard

--- src/js/components/LineItemHeader.js
+++ src/js/components/LineItemHeader.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react'
 import HeaderPlaceholder from '../../pics/header-placeholder.png'
 
 export default class LineItemHeader extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
   }
+
   render () {
     return (
       <div className='vdb-line-item-header_container'>
         <div className={this.props.lineItemTop ? 'vdb-line-item-header is-top' : 'vdb-line-item-header'} style={{backgroundImage: `url(${HeaderPlaceholder})`}}>
-          <div className='vdb-line-item-header_scroll' style={{opacity: this.props.lineItemScroll/100}}></div>
+          <div className='vdb-line-item-header_scroll' style={{opacity: this.props.lineItemScroll / 100}}></div>
           <div className='vdb-line-item-header_infos'>
             <h3 className='name'>
               <span className='number'><span>200</span></span>
