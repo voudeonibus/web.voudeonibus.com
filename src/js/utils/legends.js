c7a50e172a7c0dd56597b43b8265dd154b03bcc3
Carlos Alexandre Fuechter <alexandref93@gmail.com>
2016-11-12 15:03:31 -0200

    fix variations undefined

--- src/js/utils/legends.js
+++ src/js/utils/legends.js
@@ -1,5 +1,11 @@
 import _ from 'lodash'
 
 export function maskLegend(item) {
-  return `${item.origin} / ${item.destin} - ${item.variations.join(' - ')}`
+
+  let variations = ''
+  if (item.variations.length > 0) {
+    variations = `- ${item.variations.join(' - ')}`
+  }
+
+  return `${item.origin} / ${item.destin} ${variations}`
 }
